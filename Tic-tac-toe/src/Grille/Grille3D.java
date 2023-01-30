aefa0f728f694b3adaf2c2136e9914795e9c9b67
lolokokoo <mano.raichon@etu.univ-lyon1.fr>
2023-01-30 10:42:27 +0100

    3D

--- Tic-tac-toe/src/Grille/Grille3D.java
+++ Tic-tac-toe/src/Grille/Grille3D.java
@@ -36,6 +36,20 @@ public class Grille3D {
 	}
 	
 	public void afficherGrille() {
+		//On affiche les lettre correspondant aux num des grilles
+		System.out.print("  ");
+		for (int i = 0; i <taille; i++) {
+			char c = (char) (i + 'a');
+			String s = Character.toString(c);
+			System.out.print(s);
+			int espace = 0;
+			while (espace < taille*3 -1) {
+				System.out.print(" ");
+				espace++;
+			}
+		}
+		System.out.println("");
+		//On affiche les n grilles
 		for (int ligne = 0; ligne < taille; ligne++) {
 			System.out.print("| ");
 			for (int profondeur = 0; profondeur < taille; profondeur++) {
@@ -67,6 +81,19 @@ public class Grille3D {
 	        throw new IllegalArgumentException("Les indices de ligne, colonne et profondeur doivent être compris entre 0 et " + (this.taille - 1));
 	    }
 	    cube[ligne][colonne][profondeur] = symbole;
+	}
+	public void placerPion(int chiffre, String lettre, Joueur currentplayer) {
+		
+	    int profondeur = lettre - 'a' + 1;
+	    if (chiffre < 1 || chiffre > this.taille * this.taille) {
+	        throw new IllegalArgumentException("Le chiffre doit être compris entre 1 et " + this.taille * this.taille);
+	    }
+	    int colonne = (chiffre - 1) / this.taille;
+	    int ligne = (chiffre - 1) % this.taille;	    
+	    if (!(this.getCase(ligne, colonne).equals(Integer.toString(chiffre)))) {
+	        throw new IllegalArgumentException("La case " + chiffre + " est déjà occupée");
+	    }
+	    this.setCase(currentplayer.getSymbole(), ligne, colonne);
 	}
 }
 
--- Tic-tac-toe/src/jeucommun/Main.java
+++ Tic-tac-toe/src/jeucommun/Main.java
@@ -5,12 +5,11 @@ import deroulementJeu.*;
 public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Grille2D Grille = new Grille2D(3);
-		Grille.afficherGrille();
-		Jeu jeu = new Jeu2D(Grille);
-		jeu.deroulementJeu();
-		//Grille3D Grille = new Grille3D(3);
+		//Grille2D Grille = new Grille2D(3);
 		//Grille.afficherGrille();
-		//System.out.println(a[0][1][0]);
+		//Jeu jeu = new Jeu2D(Grille);
+		//jeu.deroulementJeu();
+		Grille3D Grille = new Grille3D(3);
+		Grille.afficherGrille();
 	}
 }
